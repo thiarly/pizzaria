c66e05bf7fc5fbe0dfedb49b7716edc7186088e8
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-06 15:43:19 -0300

    começando projeto

---
+++ backend/src/routes.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/', (request: Request, response: Response) => {
+  return response.json({ message: 'Hello World!' });
+});
+
+export { router }
