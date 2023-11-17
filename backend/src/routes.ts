e49c4776f14309fe158a6ecb13f585ed7d7c5665
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-17 09:52:54 -0300

    Criando models

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,8 +2,8 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (request: Request, response: Response) => {
-  throw new Error('Error message test');
+router.get('/teste', (request: Request, response: Response) => {
+    return response.json({ message: 'Hello World' });
 });
 
 export { router }
