f4cb6f9c55c32f05477084f4e2aa0d9acf671f94
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-23 10:20:43 -0300

    Criando nosso document

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>TC Pizzaria - Faça seu login</title>
+    </Head>
     <div>
-      <h1>HOME PAGE</h1>
+      <h1>TC Pizzaria Home Page</h1>
     </div>
+    </>
   )
 }
