73dea97acbc0ab488ceaf9da98a7923ada223ee7
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-24 11:57:38 -0300

    Configurando cadastro

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,3 +1,5 @@
+import { useState, FormEvent } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../../styles/home.module.scss'
@@ -10,6 +12,24 @@ import { Button } from '../../components/ui/Button'
 import Link from 'next/link'
 
 export default function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false)
+
+  async function handleSignUp(event: FormEvent) {
+    event.preventDefault()
+
+    if (name === '' || email === '' || password === '') {
+      alert ('Preencha todos os campos')
+      return;
+    }
+
+    setLoading(true)
+
+  }
+
   return (
     <>
     <Head>
@@ -20,22 +40,28 @@ export default function SignUp() {
 
       <div className={styles.login}>
         <h1>Criando sua conta</h1>
-        <form>
+        <form onSubmit={handleSignUp}>
           <Input
             placeholder='Digite seu nome'
-            type='email'
+            type='text'
+            value={name}
+            onChange={event => setName(event.target.value)}
           />
           <Input
             placeholder='Digite seu email'
-            type='password'
+            type='email'
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
             <Input
             placeholder='Digite sua senha'
             type='password'
+            value={password}
+            onChange={event => setPassword(event.target.value)}
           />
           <Button
             type='submit'
-            loading={false}
+            loading={loading}
           >
             Cadastrar
           </Button>
