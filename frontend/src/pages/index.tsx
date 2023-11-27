1a76fd1bf5d1916177b4fe91daf3109d8fbd9691
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-27 11:34:43 -0300

    Rotas privadas

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -15,6 +15,8 @@ import {toast } from 'react-toastify'
 
 import Link from 'next/link'
 
+import  { canSSRGuest } from '../utils/canSSRGuest';
+
 export default function Home() {
   const { signIn } = useContext(AuthContext)
 
@@ -83,3 +85,10 @@ export default function Home() {
     </>
   )
 }
+
+
+export const getServerSideProps = canSSRGuest(async (context) => {
+  return {
+    props: {}
+  }
+})
