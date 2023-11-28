5b30210fda21b840efb53a289ec46820edc0a3b0
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-28 17:22:11 -0300

    Configurando nossa navegação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
 
-import SignIn from './src/pages/SignIn';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <NavigationContainer>
       <StatusBar backgroundColor="#1d1d2e" barStyle="light-content" translucent={false} />
-      <SignIn />
-    </SafeAreaView>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1d1d2e', // Ajuste conforme necessário
-  },
-});
