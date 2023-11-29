8f7c4b530c2f7f162ebcc1333d38c54e817a52a1
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-29 11:47:17 -0300

    Recebendo params

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { useRoute, RouteProp } from "@react-navigation/native";
+
+type RouteDetailParams ={
+    Order: {
+        mesa: string | number;
+        order_id: string | number;
+    };
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, "Order">;
 
 export default function Order() {
+    const route = useRoute<OrderRouteProps>();
+
     return (
         <View style={styles.container}>
             <Text>Tela de Pedidos</Text>
+            <Text>
+                {route.params.mesa}
+            </Text>
         </View>
     );
 }
