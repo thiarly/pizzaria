fbfd7dcb61e723e1709c22992767c9e941043c17
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-12-05 18:01:52 -0300

    update logo

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:3333',
-  baseURL: 'http://172.16.100.100:3333',
+  baseURL: 'http://192.168.0.5:3333',
 });
 
 
